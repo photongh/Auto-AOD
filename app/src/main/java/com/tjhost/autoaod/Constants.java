3d313930f9c733a09b07845c7500ada9062b64eb
allen <allen.j.tang@foxconn.com>
2019-05-31 14:41:29 +0800

    change airplane mode setting default value to off

--- app/src/main/java/com/tjhost/autoaod/Constants.java
+++ app/src/main/java/com/tjhost/autoaod/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
     public static final boolean DEBUG = false; //BuildConfig.DEBUG;
 
     public static final boolean DEFAULT_SETTING_SERVICE_ENABLE = false;
-    public static final boolean DEFAULT_SETTING_AIRMODE_ENABLE = true;
+    public static final boolean DEFAULT_SETTING_AIRMODE_ENABLE = false;
 
 }
